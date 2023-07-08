8a9b5e4f41553826d2fe3f60606e9928a21caedf
Mirza Sahid Afridi <mirzashahidafridi@gmail.com>
2023-07-09 01:27:33 +0800

    First commit

---
+++ RainfallDataProcessing/src/rainfall/RainfallDataConsumer.java
@@ -0,0 +1,72 @@
+package rainfall;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * The RainfallDataConsumer class is responsible for consuming the rainfall data
+ * from a file created by RainfallDataCreator, computing the average rainfall,
+ * and displaying the data.
+ */
+public class RainfallDataConsumer {
+    /**
+     * The main method reads the rainfall data file, computes the average rainfall,
+     * and displays the data and average.
+     *
+     * @param args the command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        String fileName = "rainfall_data.bin"; // Location of the data file
+        
+        try {
+            consumeRainfallData(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads the rainfall data from the specified file, computes the average rainfall,
+     * and displays the data and average.
+     *
+     * @param fileName the name of the file containing the rainfall data
+     * @throws IOException if an I/O error occurs while reading the file
+     */
+    private static void consumeRainfallData(String fileName) throws IOException {
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(fileName))) {
+            String stationName = dis.readUTF(); // Read station name as UTF string
+            String district = dis.readUTF(); // Read district as UTF string
+            int numDays = dis.readInt(); // Read the number of days of rainfall data
+
+            int[] rainfallData = new int[numDays];
+            for (int i = 0; i < numDays; i++) {
+                rainfallData[i] = dis.readInt(); // Read each rainfall data as Integer
+            }
+
+            System.out.println("Station Name: " + stationName);
+            System.out.println("District: " + district);
+            System.out.println("Rainfall Data: ");
+            for (int rainfall : rainfallData) {
+                System.out.println(rainfall);
+            }
+
+            int averageRainfall = computeAverageRainfall(rainfallData);
+            System.out.println("Average Rainfall: " + averageRainfall);
+        }
+    }
+
+    /**
+     * Computes the average rainfall from the given rainfall data.
+     *
+     * @param rainfallData an array of rainfall data
+     * @return the average rainfall
+     */
+    private static int computeAverageRainfall(int[] rainfallData) {
+    	int sum = 0;
+        for (int rainfall : rainfallData) {
+            sum += rainfall;
+        }
+        return sum / rainfallData.length;
+    }
+}
---
+++ RainfallDataProcessing/src/rainfall/RainfallDataCreator.java
@@ -0,0 +1,69 @@
+package rainfall;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * The RainfallDataCreator class is responsible for creating a set of rainfall data
+ * and storing it in a file using java.io.DataOutputStream.
+ */
+public class RainfallDataCreator {
+    private String fileName;
+    private String stationName;
+    private String district;
+    private int[] rainfallData;
+
+    /**
+     * Constructs a RainfallDataCreator object with the specified parameters.
+     *
+     * @param fileName      the name of the file to store the rainfall data
+     * @param stationName   the name of the weather station
+     * @param district      the district where the weather station is located
+     * @param rainfallData  an array of integer values representing the rainfall data
+     */
+    public RainfallDataCreator(String fileName, String stationName, String district, int[] rainfallData) {
+        this.fileName = fileName;
+        this.stationName = stationName;
+        this.district = district;
+        this.rainfallData = rainfallData;
+    }
+
+    /**
+     * Creates the rainfall data file and stores the data using java.io.DataOutputStream.
+     * The data includes the station name, district, and the rainfall readings.
+     */
+    public void createRainfallData() {
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(fileName))) {
+            dos.writeUTF(stationName); // Write station name as UTF string
+            dos.writeUTF(district); // Write district as UTF string
+            
+            dos.writeInt(rainfallData.length); // Write the length of the data array
+            
+            for (int rainfall : rainfallData) {
+                dos.writeInt(rainfall); // Write each rainfall data as integer
+            }
+            
+            System.out.println("Data created Successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * The main method creates an instance of RainfallDataCreator and calls the
+     * createRainfallData() method to create the rainfall data file.
+     *
+     * @param args the command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        String fileName = "rainfall_data.bin"; // File location
+        String stationName = "Durian Tunggal"; // Station name
+        String district = "Alor Gajah"; // District
+        int[] rainfallData = {0, 0, 0, 26, 3, 1}; // Rainfall data of Alor Gajah in 6 days
+        
+        // Creating an instance and calling the method
+        RainfallDataCreator dataCreator = new RainfallDataCreator(fileName, stationName, district, rainfallData);
+        dataCreator.createRainfallData();
+    }
+}
